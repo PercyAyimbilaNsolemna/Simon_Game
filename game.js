59f14e6ea4eabc6fc51f037dc87522b211194c9f
master <pnayimbila@st.ug.edu.gh>
2023-04-26 20:55:31 +0000

    Created an empty array called gamePattern.

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 
+var gamePattern = [];
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var randomNumber = newSequence();
 
 var randonChosenColour = buttonColours[randomNumber];
 
-// console.log(randonChosenColour);
 
 function newSequence(){
     var randomNumber = Math.random() * 4;

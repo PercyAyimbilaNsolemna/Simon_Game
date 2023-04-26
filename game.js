f90857f8a88c7256aeb15c11f9bf1558b107ccdf
master <pnayimbila@st.ug.edu.gh>
2023-04-26 20:51:37 +0000

    Created a newSequence function.

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-alert("Working!");
+
+// var buttonColours = ["red", "blue", "green", "yellow"];
+
+// var randomNumber = newSequence();
+
+// var randonChosenColour = buttonColours[randomNumber];
+
+// console.log(randonChosenColour);
+
+function newSequence(){
+    var randomNumber = Math.random() * 4;
+    randomNumber = Math.floor(randomNumber);
+    return randomNumber;
+}
